e2cd982fb67347eb2a1a86aa0954e6e444472cf4
Adolfo Neto <adolfo.usp@gmail.com>
2019-07-18 18:35:33 -0300

    A simple and not efficient implementation of linear tableau explasion

--- lib/cpl.ex
+++ lib/cpl.ex
@@ -22,4 +22,99 @@ defmodule Cpl do
   end
 
   def is_formula(_, _), do: false
+
+  def apply([:t, [:not, x]]) do
+    [[:f, x]]
+  end
+
+  def apply([:f, [:not, x]]) do
+    [[:t, x]]
+  end
+
+  def apply([:t, [x, :and, y]]) do
+    [[:t, x], [:t, y]]
+  end
+
+  def apply([:f, [x, :or, y]]) do
+    [[:f, x], [:f, y]]
+  end
+
+  def apply([:f, [x, :implies, y]]) do
+    [[:t, x], [:f, y]]
+  end
+
+  def apply([:f, [_, :and, _]]) do
+  end
+
+  def apply([:t, [_, :or, _]]) do
+  end
+
+  def apply([:t, [_, :implies, _]]) do
+  end
+
+  def apply([:t, x]) when is_atom(x) do
+  end
+
+  def apply([:f, x]) when is_atom(x) do
+  end
+
+  def apply(_) do
+    :error
+  end
+
+  def expand_tableau(tableau) do
+    tableau
+    |> Enum.map(&Cpl.apply/1)
+    |> Enum.filter(fn x -> x != nil end)
+  end
+
+  def apply_branching_rule([:f, [x, :and, y]]) do
+    {[:f, x], [:f, y]}
+  end
+
+  def apply_branching_rule([:t, [x, :or, y]]) do
+    {[:t, x], [:t, y]}
+  end
+
+  def apply_branching_rule([:t, [x, :implies, y]]) do
+    {[:f, x], [:t, y]}
+  end
+
+  defp my_flatten_aux([[head1, head2] | tail], partial) do
+    my_flatten_aux(tail, [head2, head1] ++ partial)
+  end
+
+  defp my_flatten_aux([[head] | tail], partial) do
+    my_flatten_aux(tail, [head] ++ partial)
+  end
+
+  defp my_flatten_aux([], partial) do
+    partial
+  end
+
+  defp my_flatten(list) do
+    Enum.reverse(my_flatten_aux(list, []))
+  end
+
+  defp expand_all_aux(tableau, partial_result) do
+    temp = expand_tableau(tableau)
+    # IO.puts("temp:")
+    # IO.inspect(temp)
+
+    temp = my_flatten(temp)
+    # IO.puts("my_flatten(temp):")
+    # IO.inspect(temp)
+
+    cond do
+      temp == [] ->
+        tableau ++ partial_result
+
+      true ->
+        expand_all_aux(temp, tableau ++ partial_result)
+    end
+  end
+
+  def expand_all(tableau) do
+    expand_all_aux(tableau, [])
+  end
 end
--- test/cpl_test.exs
+++ test/cpl_test.exs
@@ -33,4 +33,89 @@ defmodule CplTest do
   test "[:not, [ [ [:p, :implies, :q], :and, :r], :or, :s]] is a well-formed formula" do
     assert is_formula([:not, [[[:p, :implies, :q], :and, :r], :or, :s]], @lp)
   end
+
+  test "All NOT rules" do
+    assert apply([:t, [:not, :p]]) == [[:f, :p]]
+    assert apply([:f, [:not, :p]]) == [[:t, :p]]
+  end
+
+  test "linear binary rules" do
+    assert apply([:t, [:p, :and, :q]]) == [[:t, :p], [:t, :q]]
+    assert apply([:f, [:p, :or, :q]]) == [[:f, :p], [:f, :q]]
+    assert apply([:f, [:p, :implies, :q]]) == [[:t, :p], [:f, :q]]
+  end
+
+  test "atomic formulas" do
+    assert apply([:t, :p]) == nil
+    assert apply([:f, :q]) == nil
+  end
+
+  test "Error" do
+    assert apply(:t) == :error
+    assert apply([:y, :q]) == :error
+  end
+
+  test "Expand Tableau 1" do
+    tableau = [
+      [:t, :a],
+      [:t, [:a, :implies, :b]],
+      [:f, :b]
+    ]
+
+    assert expand_tableau(tableau) == []
+  end
+
+  test "Expand Tableau 2" do
+    tableau = [
+      [:t, [:a, :and, :b]],
+      [:f, [:a, :implies, :b]],
+      [:f, [:a, :or, :b]],
+      [:t, [:not, :a]],
+      [:f, [:not, :a]]
+    ]
+
+    assert expand_tableau(tableau) == [
+             [[:t, :a], [:t, :b]],
+             [[:t, :a], [:f, :b]],
+             [[:f, :a], [:f, :b]],
+             [[:f, :a]],
+             [[:t, :a]]
+           ]
+  end
+
+  test "Expand Tableau 3" do
+    tableau = [
+      [:t, [:not, [:not, :a]]]
+    ]
+
+    assert expand_tableau(tableau) == [
+             [[:f, [:not, :a]]]
+           ]
+
+    [x] = expand_tableau(tableau)
+    assert expand_tableau(x) == [[[:t, :a]]]
+  end
+
+  test "Expand all" do
+    tableau = [
+      [:t, [:not, [:not, :a]]],
+      [:t, [:b, :and, :c]]
+    ]
+
+    assert MapSet.new(expand_all(tableau)) ==
+             MapSet.new([
+               [:t, [:not, [:not, :a]]],
+               [:f, [:not, :a]],
+               [:t, :a],
+               [:t, [:b, :and, :c]],
+               [:t, :b],
+               [:t, :c]
+             ])
+  end
+
+  test "branching rules" do
+    assert apply_branching_rule([:f, [:p, :and, :q]]) == {[:f, :p], [:f, :q]}
+    assert apply_branching_rule([:t, [:p, :or, :q]]) == {[:t, :p], [:t, :q]}
+    assert apply_branching_rule([:t, [:p, :implies, :q]]) == {[:f, :p], [:t, :q]}
+  end
 end
